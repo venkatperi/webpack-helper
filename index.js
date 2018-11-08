10e229b1f93ae5220fb5f77bd4b5288b5eaadff7
Venkat Peri <venkatperi@gmail.com>
2018-11-08 11:42:44 -0500

    updates

--- index.js
+++ index.js
@@ -1,31 +1,5 @@
-const path = require( 'path' )
-const run = require( './lib/index' )
-
-const defaults = {
-  modules: [
-    'vue',
-    'ts',
-    ['style', {
-      cssLoader: {
-        modules: true,
-        localIdentName: '[folder]-[name]-[local]',
-      },
-    }],
-    'img',
-    'ext',
-    'devServer',
-    'misc',
-    'dev',
-    'prod',
-  ],
-
-  variants: [
-    'lib',
-    'umd',
-  ],
-}
+const webpackHelper = require( './lib/index' )
 
 module.exports = {
-  run,
-  defaults,
+  webpackHelper,
 }
