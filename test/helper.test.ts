fb71fa5ad92712d02fc110c8c16e54c97fcae5ba
Venkat Peri <venkatperi@gmail.com>
2018-11-13 06:39:43 -0500

    updates

--- test/helper.test.ts
+++ test/helper.test.ts
@@ -9,9 +9,9 @@ let buildDir = 'dist'
 
 describe('webpack helper', () => {
 
-    it('generates a webpack config function', () => {
-        expect(webpackHelper([])).to.be.a.instanceOf(Function)
-    })
+    // it('generates a webpack config function', () => {
+    //     expect(webpackHelper([])).to.be.a.instanceOf(Function)
+    // })
 
 
     // it('validates args', async () => {
